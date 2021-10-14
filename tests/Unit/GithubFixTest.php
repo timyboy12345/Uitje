bd1f85ad1063ea6bea13b6250fbe33b733c168ae
Tim Arendsen <t.j.m.arendsen@gmail.com>
2021-10-14 15:51:43 +0200

    Added a single Unit test

---
+++ tests/Unit/GithubFixTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class GithubFixTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     * This test is necessary because the folder is otherwise not uploaded to Git
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
