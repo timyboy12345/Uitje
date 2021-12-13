ec8503752ef96efba3b9c46e70ede6d19d83a0f4
Tim Arendsen <t.j.m.arendsen@gmail.com>
2021-12-13 08:33:58 +0100

    Fixed test workflow, fixed deployment

--- resources/lang/nl-NL/dashboard/content/shop.php
+++ resources/lang/nl-NL/dashboard/content/shop.php
@@ -4,6 +4,7 @@ return [
     'index' => [
         'subtitle' => 'Beheer op deze pagina de look and feel van je winkel.',
         'show-site-link' => 'Ga naar je webshop op :url',
+        'card-title' => 'Algemene winkelgerelateerde-instellingen',
     ],
     'edit' => []
 ];
--- resources/views/dashboard/content/shop/index.blade.php
+++ resources/views/dashboard/content/shop/index.blade.php
@@ -1,12 +1,10 @@
 @extends('layouts.dashboard')
 @section('title', __('general.terms.shop'))
 @section('subtitle', __('dashboard/content/shop.index.subtitle'))
-@section('enableSearch', true)
-@section('createDestination', route('dashboard.content.pois.create'))
 
 @section('content')
     <div class="lg:col-span-2">
-        <x-dashboard.card>
+        <x-dashboard.card :title="__('dashboard/content/shop.index.card-title')">
             <h3 class="text-indigo-800">{{ __('general.terms.url') }}</h3>
             <div class="text-gray-600 text-sm">
                 <a href="{{ $organization->subdomain }}.{{ str_replace('http://', '', config('app.url')) }}" target="_blank">
